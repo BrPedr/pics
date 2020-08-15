2a96d0b73f6d8f246a3cbd03e1e81ed8b89da496
brunoPedrosa <brunofellipep@gmail.com>
2020-08-15 17:24:05 -0300

    searching data from unsplash ready through axios

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,24 @@
 import React from "react";
-
+import axios from "axios";
 import SearchBar from "./SearchBar";
 
-const App = () => {
-  return (
-    <div className="ui container" style={{ marginTop: "10px" }}>
-      <SearchBar />
-    </div>
-  );
-};
+class App extends React.Component {
+  onSearchSubmit(term) {
+    axios.get("https://api.unsplash.com/search/photos", {
+      params: { query: term },
+      headers: {
+        Authorization: "Client-ID Br9DLxHi44gAttsTqCRkLz3p_oYsbkNbNU7fZvN8NSc",
+      },
+    });
+  }
+
+  render() {
+    return (
+      <div className="ui container" style={{ marginTop: "10px" }}>
+        <SearchBar onSubmit={this.onSearchSubmit} />
+      </div>
+    );
+  }
+}
 
 export default App;
